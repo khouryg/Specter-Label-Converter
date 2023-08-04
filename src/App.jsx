ad44524a336636c6d8042c8dba40cf7fa19a9715
George Khoury <gke@sent.com>
2023-08-03 20:41:43 -0700

    completed basic functionality of exporting tx and address labels

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,77 @@ import './App.css'
 
 function App() {
 
-  const readFile = (file) => {
-    const input = document.getElementById(file);
-    const reader = new FileReader();
-    reader.readAsText(input.files[0]);
-    reader.onload = async function () {
-      const text = reader.result;
-      const unformattedJson = JSON.parse(text);
-      const formattedJson = formatter(unformattedJson);
-      console.log(unformattedJson);
-      createAndSaveFile(formattedJson);
-    };
+  const convert = async () => {
+    let result = '';
+    const csvContent = await readFile('csvFile');
+    const walletContent = await readFile('walletFile');
+    const formattedCsvData = formatCsvFile(csvContent);
+    const formattedWalletData = formatWalletFile(walletContent);
+    const combinedData = formattedCsvData + '\n' + formattedWalletData;
+    createAndSaveFile(combinedData);
+  }
+
+  const readFile = (id) => {
+    return new Promise((resolve, reject) => {
+      if (document.getElementById(id).files.length === 0) {
+        resolve('');
+      } else {
+        const input = document.getElementById(id);
+        const reader = new FileReader();
+        reader.readAsText(input.files[0]);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = () => reject(reader.error);
+      }
+    })
+  }
+
+  const formatCsvFile = (file) => {
+    let output = '';
+    const arr = file.trimEnd().split('\n');
+    const dataArr = arr.slice(1);
+    const resultArr = [];
+    dataArr.forEach((line) => {
+      resultArr.push(line.split(','));
+    })
+    resultArr.forEach((line) => {
+      const obj = {};
+      obj['type'] = 'tx';
+      obj['ref'] = line[6];
+      obj['label'] = line[1];
+      output += JSON.stringify(obj) + '\n';
+    });
+    return output.trimEnd();
+  }
+
+  const formatWalletFile = (file) => {
+    let formattedJson = '';
+    file = JSON.parse(file);
+    // add ref for addresses and labels
+    if (file.labels) {
+      Object.keys(file.labels).forEach((label) => {
+        file.labels[label].forEach((ref) => {
+          formattedJson += JSON.stringify({
+            "type": 'addr',
+            "ref": ref,
+            "label": label
+          }) + '\n';
+        })
+      });
+    }
+    if (file.keys) {
+      file.keys.forEach((key) => {
+        formattedJson += JSON.stringify({
+          "type": 'xpub',
+          "ref": key.original,
+          "label": key.fingerprint
+        }) + '\n';
+      });
+    }
+    return formattedJson.trimEnd();
   }
 
   const createAndSaveFile = (content) => {
+    if (!content) return;
     const blob = new Blob([content], { type: 'application/json' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
@@ -30,40 +87,16 @@ function App() {
     URL.revokeObjectURL(url);
   }
 
-  const formatter = (json) => {
-    let formattedJson = '';
-    // add ref for addresses and labels
-    if (json.labels) {
-      Object.keys(json.labels).forEach((label) => {
-        json.labels[label].forEach((ref) => {
-          formattedJson += JSON.stringify({
-            "type": 'addr',
-            "ref": ref,
-            "label": label
-          }) + '\n';
-        })
-      });
-    }
-    if (json.keys) {
-      console.log(json.keys);
-      json.keys.forEach((key) => {
-        formattedJson += JSON.stringify({
-          "type": 'xpub',
-          "ref": key.original,
-          "label": key.fingerprint
-        }) + '\n';
-      });
-    }
-
-    return formattedJson.trimEnd();
-  }
 
   return (
     <>
       <h1>Sparrow Label Converter</h1>
-      <input type='file' id='fileInput' accept='.json'></input>
+      <h3>Sparrow Wallet Export File</h3>
+      <input type='file' id='walletFile' accept='.json'></input>
+      <h3>Sparrow Transactions CSV</h3>
+      <input type='file' id="csvFile" accept='.csv'></input>
       <div id='convertButton--div'>
-        <button id='convertButton' onClick={() => { readFile('fileInput') }} >Convert</button>
+        <button id='convertJsonButton' onClick={() => { convert() }} >Convert</button>
       </div>
     </>
   )
